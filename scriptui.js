86c83ebcc5e28c2091a8e9fd04778ef347836410
KoryakinaAnastasiia <koryakinaanastasiia@gmail.com>
2026-03-16 15:46:07 +0300

    js buttonsLike

--- scriptui.js
+++ scriptui.js
@@ -1,1 +1,13 @@
-const 
+const buttonLike = document.querySelector(".buttonsLike__input");
+const hartElements = document.querySelector(".buttonsLike__counter");
+
+let count = parseInt(hartElements.textContent);
+
+buttonLike.addEventListener("change", () => {
+  if (buttonLike.checked) {
+    count++;
+  } else {
+    count--;
+  }
+  hartElements.textContent = count;
+});
