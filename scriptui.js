24d4ee8f3de483115c531e2446f7836be721e2f9
KoryakinaAnastasiia <koryakinaanastasiia@gmail.com>
2026-03-20 20:15:17 +0300

    comments

--- scriptui.js
+++ scriptui.js
@@ -1,30 +1,32 @@
 /* like buttons */
-const buttonLike = document.querySelector(".buttonsLike__input");
-const hartElements = document.querySelector(".buttonsLike__counter");
+const buttonLikeInput = document.querySelector(".buttonsLike__input");
+//const likesCount = document.querySelector(".buttonsLike__counter");
 
-let count = parseInt(hartElements.textContent);
+//let count = parseInt(likesCount.textContent);
 
-buttonLike.addEventListener("change", () => {
-  if (buttonLike.checked) {
+buttonLikeInput.addEventListener("change", (event) => {
+  const input = event.target; // почитать
+  const likesCount = input.parentElement.querySelector(".buttonsLike__counter");
+  let count = parseInt(likesCount.textContent);
+  if (buttonLikeInput.checked) {
     count++;
   } else {
     count--;
   }
-  hartElements.textContent = count;
+  likesCount.textContent = count;
 });
 
 /* Rate Buttons */
 
 const buttons = document.querySelectorAll(".buttonStars");
 // Выбираем все залитые звезды
-const starsOn = document.querySelectorAll(".buttonsRate__stars");
-// Выбираем все звезды
 
 buttons.forEach((btn, index) => {
   btn.addEventListener("click", () => {
+    // Выбираем все звезды
+    const starsOn = document.querySelectorAll(".buttonsRate__stars");
     // Сбрасываем активный класс у всех залитых звезд
     starsOn.forEach((star) => star.classList.remove("active"));
-
     // Красим все звезды ДО нажатой включительно
     for (let i = 0; i <= index; i++) {
       starsOn[i].classList.add("active");
